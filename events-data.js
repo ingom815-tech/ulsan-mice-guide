41c0ce38e6df0a3725587eeabae280085207aa6d
ingom815-tech <ingom815@gmail.com>
2026-08-07 15:59:56 +0900

    Add files via upload

--- events-data.js
+++ events-data.js
@@ -6,7 +6,7 @@ const EVENTS = [
   {id:"kalgs", poster:"images/banner-hya.jpg", programs:[{id:"trevier", days:"8. 28.(금)", daysEn:"Aug 28 (Fri)", time:"운영시간 추후 공지", timeEn:"Hours TBA"}], moreComing:true, ko:"한국지방정부학회 하계학술대회", en:"KALGS Summer Conference",
    s:"2026-08-28", e:"2026-08-28", vko:"유에코", ven:"UECO", month:8,
    shuttle:{
-     stops:[{ko:"울산역 (KTX)",en:"KTX Ulsan Station"},{ko:"UECO",en:"UECO",hub:true}],
+     stops:[{ko:"울산역 (KTX)",en:"KTX Ulsan Station",c:[35.551431,129.138506],icon:"train",dir:"bottom"},{ko:"UECO",en:"UECO",hub:true,c:[35.555695,129.131635],icon:"hub",dir:"top"}],
      hoursKo:"오전 11:30 ~ 오후 8:20 (행사 기간 중)", hoursEn:"11:30 AM – 8:20 PM (during the event)",
      table:[
        {dko:"울산역(KTX) → UECO", den:"Ulsan Stn (KTX) → UECO", t:["11:30","11:50","12:10","12:30","12:50"]},
